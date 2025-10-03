ef884924a3b7344beb13cda121793fb0ec8553d7
junayed0912 <junayedsheikh94@gmail.com>
2025-10-03 20:59:08 +0600

    pay bill html

--- script/toggle.js
+++ script/toggle.js
@@ -2,6 +2,6 @@ document.getElementById('latest-payment-section').style.display = 'flex';
 document.getElementById('add-money-section').style.display = 'none'; 
 document.getElementById('cashout-section').style.display = 'none';
 document.getElementById('transaction-section').style.display = 'block';
-document.getElementById('transfer-money-section').style.display = 'block';
+document.getElementById('transfer-money-section').style.display = 'none';
 document.getElementById('bonus-section').style.display = 'none';
 console.log("toggle.js is connected");
